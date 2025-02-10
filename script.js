458c939e2b47b622acb429a1824eb23588bb0f72
PraveenKumar706 <praveenkumar7060901251@gmail.com>
2025-02-10 20:25:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+/*************** typing animation  *****************/
+var typed = new Typed(".typing",{
+    Strings:["web Designer","Graphic Designer","Front End Devloper"],
+    typespeed:100,
+    loop:true
+})
